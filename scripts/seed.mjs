7dee791f0be333b66769f97e269aae28eb10fc6c
Pranav Sai Bhagath <pranavsaibhagath@gmail.com>
2026-07-24 22:21:40 +0530

    Update seed.mjs

--- scripts/seed.mjs
+++ scripts/seed.mjs
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import fs from "fs";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
